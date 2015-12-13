e221f708728ce97464604fc49d6e2e413695372f
amartine <aj01_martinez@yahoo.com>
2015-12-13 17:03:27 -0600

    update ai

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -4,35 +4,41 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour
 {
 	private NavMeshAgent agent;
-	private Transform destinationPoint;
-	private Transform exitPoint;
+	public Transform destinationPoint;
+	public Transform exitPoint;
 	public bool isHealthy = false;
+	public bool exitFlag = false;
 
 	// Use this for initialization
 	void Start ()
 	{
 		agent = gameObject.GetComponent<NavMeshAgent> ();
-		destinationPoint = GameObject.FindWithTag ("Destination").transform;
-		exitPoint = GameObject.FindWithTag ("Exit").transform;
 	}
 
 
 	void OnTriggerEnter(Collider other){
-		if (other.name == "Exit") {
-			Destroy(gameObject);
+		if (other.name == "Cheeseburger") {
+			//lose heart and customer exits.
+			exitFlag = true;
+		} else if (other.name == "Exit") {
+			Destroy (gameObject);
+		} else if (other.name == "Apple") {
+			isHealthy = true;
+			ScoreManager.score += 10;
+		} else if (other.name == "Orange") {
+			ScoreManager.score += 15;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (!isHealthy) {
-			agent.SetDestination(destinationPoint.position);
-		} else {
-			agent.SetDestination(transform.position);
+		if(exitFlag) {
 			//play eating animation and happy. then customer exits.
 			agent.SetDestination(exitPoint.position);
-		}
+		} else if (!isHealthy) {
+			agent.SetDestination(destinationPoint.position);
+		} 
 	}
 }
 
